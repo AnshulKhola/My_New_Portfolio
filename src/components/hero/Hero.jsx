025b04dd7ef5e7fab0b4b2cea99d2f8b263d9132
AnshulKhola <golukhola18@gmail.com>
2024-06-21 14:05:18 +0530

    this is new commit

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,152 @@
+import React from "react";
+import { AiFillTwitterCircle, AiFillGithub } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FiDownload } from "react-icons/fi";
+import { FaPlay } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
+import resumePDF from "../../assets/Sandip_Sarkar.pdf";
+import { BsArrowUpRightCircleFill } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+
+const Hero = () => {
+  const handleEmailButtonClick = () => {
+    if (/Mobi|Android/i.test(navigator.userAgent)) {
+      // If it's a mobile device, open the default mail client
+      window.location.href = "mailto:golukhola18@gmail.com";
+    } else {
+      // If it's not a mobile device, open Gmail in the default browser
+      window.open(
+        "https://mail.google.com/mail/?view=cm&fs=1&to=golukhola18@gmail.com",
+        "_blank"
+      );
+    }
+  };
+
+  return (
+    <div id="home" className={` `}>
+      <div className=" container mx-auto pt-5 h-[750px] md:h-[100vh] md:flex-col-reverse sm:h-[780px]  flex sm:flex-col-reverse sm:pt-0 ">
+        <div className=" left mt-4 md:mt-0 flex-1 flex flex-col justify-center gap-5 w-1/2 md:w-full md:py-2 sm:py-0">
+          <div className="info w-fit flex flex-col items-start justify-center gap-3 sm:gap-2">
+            <h2
+              data-aos="fade-up"
+              className=" text-5xl font-semibold sm:text-[2rem] font-[]"
+            >
+              Hey, I'm{" "}
+              <span className="text-yellow- font-bold font-mono">
+                Anshul Khola
+              </span>
+            </h2>
+            <TypeAnimation
+              data-aos="fade-up"
+              sequence={["Full Stack Developer", 2000," "]}
+              speed={30}
+              wrapper="h2"
+              repeat={Infinity}
+              className="text-yellow-500 text-4xl font-bold sm:text-3xl"
+            />
+            <p
+              className=" text-[1.1rem] font-medium w-3/4 md:w-full text-gray-600 sm:text-[.95rem]"
+            >
+              Final-year student at NIT Kurukshetra.{" "}
+            </p>
+          </div>
+          <div data-aos="fade-up" className="buttons flex gap-5">
+            <a
+              href="https://www.linkedin.com/in/anshulkhola/"
+              className=" bg-black text-[1rem] text-white px-10 py-2 sm:px-8 rounded-lg font-bold  hover:text-yellow-500"
+            >
+              <span> Hire Me</span>
+            </a>
+            <a
+              href={resumePDF}
+              className="flex items-center gap-2 border- text-[1rem] bg-white border-black px-7 py-2 sm:px-6 rounded-lg font-bold  hover:text-yellow-500"
+              download
+            >
+              <div className="flex items-center gap-1 relative">
+                Resume <FiDownload />
+              </div>
+            </a>
+          </div>
+          <div className="icons flex mt-5">
+            <ul
+              data-aos="fade-up"
+              data-aos-duration="1500"
+              className=" flex gap-5"
+            >
+              <li>
+                <a href="https://github.com/AnshulKhola">
+                  {" "}
+                  <AiFillGithub className=" h-[1.8rem] w-[1.8rem] text-xl hover:scale-125" />{" "}
+                </a>
+              </li>
+
+              <li>
+                <a href="https://www.linkedin.com/in/anshulkhola/">
+                  {" "}
+                  <FaLinkedinIn className=" h-[1.8rem] w-[1.8rem] text-xl hover:scale-125 " />{" "}
+                </a>
+              </li>
+              <li>
+                <a href="https://devalphaspace.com/">
+                  {" "}
+                  <BsArrowUpRightCircleFill className=" h-[1.7rem] w-[1.7rem] text-[1rem] hover:scale-125  " />{" "}
+                </a>
+              </li>
+
+              <li>
+                <a href="https://twitter.com/AnshulKhola18">
+                  {" "}
+                  <AiFillTwitterCircle className=" h-[1.8rem] w-[1.8rem] text-xl hover:scale-125  " />{" "}
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="right  top-5 flex-1 flex items-center justify-center md:items-end sm:items-end">
+          <div className="relative h-[80%] w-fit flex items-center sm:items-end">
+            <div className="h-[80%] md:h-[95%]">
+              <img
+                loading="lazy"
+                // data-aos="fade-up"
+                className="image h-[450px] w-[450px] md:h-[300px] md:w-[300px]  overflow-hidden object-cover  md:m-auto sm:m-0"
+                src="https://res.cloudinary.com/dbyf2dydg/image/upload/v1718887149/Anshul_Image_ytbxqe.jpg"
+                // src={mine}
+                alt="mine"
+              />
+            </div>
+            <div className=" absolute bottom-20 md:bottom-3 right-2 md:right-2">
+              <div
+                data-aos="zoom-in"
+                data-aos-duration="1000"
+                className=" relative cursor-pointer"
+              >
+                <img
+                  loading="lazy"
+                  className=" w-[120px] md:w-[90px] circle-text"
+                  src="https://ik.imagekit.io/imgkitt/tr:w-400/Full_Stack_Developer2.png?updatedAt=1683134009107"
+                  alt=""
+                />
+                <FaPlay className=" text-black absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* mail button */}
+      <div className="fixed bottom-10 right-10 sm:right-5 sm:bottom-5 bg-yellow-500 w-16 h-16 sm:w-12 sm:h-12 flex items-center justify-center rounded-full z-20 animate-bounce">
+        <button
+          data-aos="fade-up"
+          data-aos-duration="1200"
+          onClick={handleEmailButtonClick}
+          className="box font-medium text-white  flex items-start justify-center gap-1"
+        >
+          <MdEmail className=" rotate-[15deg] text-white text-3xl sm:text-2xl hover:scale-110 cursor-pointer" />
+          {/* <p>Email</p> */}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/data/data.js
@@ -0,0 +1,77 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: "https://res.cloudinary.com/dcsouj6ix/image/upload/v1709659831/My%20Portfolio/6_y0hhfw.png",
+    title: "Hotel Kusum Palace",
+    desc: "Created a responsive hotel website for Hotel Kusum Palace, showcasing Nepal's beauty. The website allows users to explore local attractions and book rooms with ease.",
+    link: "https://kusumpalace.com/",
+    git: "",
+  },
+  {
+    id: 2,
+    img: "https://res.cloudinary.com/dcsouj6ix/image/upload/v1709659832/My%20Portfolio/1_ny3n3m.png",
+    title: "Tarvel Agency",
+    link: "https://travel-agency-vite.netlify.app",
+    git: "",
+  },
+
+  // {
+  //   id: 3,
+  //   img: "https://res.cloudinary.com/dcsouj6ix/image/upload/v1709659832/My%20Portfolio/2_jlgxki.png",
+  //   title: "Nike UI Clone",
+  //   desc: "A Nike clone built using Next.js and Tailwind CSS.",
+  //   link: "https://nike-clones.vercel.app/",
+  //   git: "",
+  // },
+
+  // {
+  //   id: 4,
+  //   img: "https://ik.imagekit.io/imgkitt/tr:w-800/ecommerce.jpeg?updatedAt=1683134011136",
+  //   title: "Ecommerce Clone",
+  //   desc: "This is an e-commerce web application built using React JS, SCSS, Redux Toolkit, and a Dummy JSON API. It includes search functionality, as well as the ability to add and remove items from a shopping cart.",
+  //   link: "https://redux-store-app.netlify.app/",
+  //   git: "https://github.com/SandipNITA2025/react-redux-ecommerce",
+  // },
+  {
+    id: 6,
+    img: "https://res.cloudinary.com/dcsouj6ix/image/upload/v1709659832/My%20Portfolio/1_mdizwg.png",
+    title: "Blog Website",
+    desc: "A full-stack web app using ReactJS, Tailwind CSS, ExpressJS, and MongoDB. Features include user authentication and CRUD functionality for creating, editing, and deleting posts, with the ability to view posts by other users.",
+    link: "https://blog-web-app-delta.vercel.app/",
+    git: "https://github.com/SandipNITA2025/MERN-STACK---BLOG-POST-WEB",
+  },
+  {
+    id: 7,
+    img: "https://res.cloudinary.com/dcsouj6ix/image/upload/v1709659836/My%20Portfolio/7_nph0pn.png",
+    title: "Indohype: Latest News & Blogs",
+    desc: "Developed a dynamic Full Stack solution for Indohype website, featuring sleek User and Admin panels, and robust backend APIs. Excited to deliver top-notch functionality for the client's digital journey.",
+    link: "https://indo-hype-news-projects.vercel.app/",
+    git: "",
+  },
+  {
+    id: 8,
+    img: "https://res.cloudinary.com/dcsouj6ix/image/upload/v1709659829/My%20Portfolio/4_usta32.png",
+    title: "Palette: Design Your Clothes",
+    desc: "Palette is an e-commerce platform for customizing clothing designs, implemented with ReactJS, FabricJS, Redux Toolkit, and React Query, offering a seamless user experience. It was a freelance project for a client.",
+    link: "https://palette-frontend-ten.vercel.app/custom-design",
+    git: "",
+  },
+  {
+    id: 9,
+    img: "https://res.cloudinary.com/dcsouj6ix/image/upload/v1715420933/My%20Portfolio/Amazing_Portfolio_Website_yugwiq.png",
+    title: "Chuckle Chums: Kids Clothing",
+    desc: "Chuckle Chums is an e-commerce platform for kids, implemented with Next.js, TypeScript, Tailwind CSS, and React Query, offering a seamless user experience. It was a freelance project for a client.",
+    link: "https://chuckle-chums-frontend.vercel.app/",
+    git: "",
+  },
+  {
+    id: 10,
+    img: "https://res.cloudinary.com/dcsouj6ix/image/upload/v1709659829/My%20Portfolio/3_aiyeaj.png",
+    title: "Dev Alphaspace : Freelancing",
+    desc: "Discover our expertise in web & app development, UI/UX design. Transform your digital ideas into reality with our creative team.",
+    link: "https://devalphaspace.com/",
+    git: "",
+  },
+];
